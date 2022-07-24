e6c0ccb2f3ff35280d6c431d157a4ec043350b72
Gohar <goharayub2008@gmail.com>
2022-07-24 19:22:57 -0400

    Add event listeners to choice buttons, Change game-log scroll

--- scripts/game.js
+++ scripts/game.js
@@ -36,13 +36,14 @@ function getComputerChoice () {
     }
 }
 
-function gameRound(playerSelection, computerSelection) {
+function gameRoundUI(playerSelection, computerSelection) {
     /* str, str -> str
     Only works with inputs "Rock", "Paper", "Scissors" (is case sensitive).
     Plays a single round of Rock, Paper Scissors.
     Uses two inputs to evaluate whether the player (playerSelection)
     wins against the computer (computerSelection).
-    Note: Used in game().
+    Note: Used in game(). (no longer used by game)
+    Note Added functionality with the new UI version.
     Warning: The inputs are case sensitive.
     */
 
@@ -54,21 +55,26 @@ function gameRound(playerSelection, computerSelection) {
     else if (playerSelection == "Rock") {
         // Beats Scissors
         if (computerSelection == "Scissors") {
+            playerScoreIncrease();
             return winQuote("Rock", "Scissors");
         }
         // Loses against Paper
-        else if (computerSelection == "Paper") {}
+        else if (computerSelection == "Paper") {
+            computerScoreIncrease();
             return loseQuote("Rock", "Paper");
+        }
     }
 
     // If player chooses Paper
     else if (playerSelection == "Paper") {
         // Beats Rock
         if (computerSelection == "Rock") {
+            playerScoreIncrease();
             return winQuote("Paper", "Rock");
         }
         // Loses against Scissors
         else if (computerSelection == "Scissors") {
+            computerScoreIncrease();
             return loseQuote("Paper", "Scissors");
         }
     }
@@ -77,15 +83,58 @@ function gameRound(playerSelection, computerSelection) {
     else if (playerSelection == "Scissors") {
         // Beats Paper
         if (computerSelection == "Paper") {
+            playerScoreIncrease();
             return winQuote("Scissors", "Paper");
         }
         // Loses against Rock
         else if (computerSelection == "Rock") {
+            computerScoreIncrease();
             return loseQuote("Scissors", "Rock");
         }
     }
 }
 
+
+function writeTOLOG (logContent) {
+    /* str -> null 
+    Enters the text inserted into the game-log div tag.
+    */
+
+    const lineBreak = document.createElement("br");
+    const gameLog = document.querySelector(".game-log");
+    gameLog.insertAdjacentText("beforeend", logContent)
+    gameLog.appendChild(lineBreak);
+}
+
+function playerScoreIncrease () {
+    /* null -> null
+    Increases the Score in the player score box by 1.
+    */
+    const playerScoreBox = document.querySelector(".player-score-box");
+    playerScoreBox.textContent = parseInt(playerScoreBox.textContent) + 1;
+}
+
+function computerScoreIncrease () {
+    /* null -> null
+    Increases the Score in the computer score box by 1.
+    */
+    const computerScoreBox = document.querySelector(".computer-score-box");
+    computerScoreBox.textContent = parseInt(computerScoreBox.textContent) + 1;
+}
+
+const rockButton = document.querySelector(".rock-button");
+const paperButton = document.querySelector(".paper-button");
+const scissorsButton = document.querySelector(".scissors-button");
+
+rockButton.addEventListener("click", e => writeTOLOG
+    (gameRoundUI("Rock", getComputerChoice()))
+);
+paperButton.addEventListener("click", e => writeTOLOG
+    (gameRoundUI("Paper", getComputerChoice()))
+);
+scissorsButton.addEventListener("click", e => writeTOLOG
+    (gameRoundUI("Scissors", getComputerChoice()))
+);
     
 // function game() {
 //     /* game loop.
